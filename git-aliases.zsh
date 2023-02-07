45b416d4a50ed018ce1ed8e2a6b1460b9ea2ce93
Snorre Magnus Davøen <post@snorre.io>
2023-02-07 13:25:07 +0100

    Git aliases for common operations
    Use gum (https://github.com/charmbracelet/gum) to implement some common
    git operations, among them deleting many branches, creating conventional
    commit branches, etc.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

---
+++ git-aliases.zsh
@@ -0,0 +1,35 @@
+# Git create branch, bubble choice between conventional commit types
+function git-cb() {
+  TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
+
+  # Pre-populate the input with the type(scope): so that the user may change it
+  NAME=$(gum input --value "$TYPE/" --placeholder "Name of this branch")
+
+  # Commit these changes
+  gum confirm "Create branch? $NAME" && git checkout -b "$NAME"
+}
+
+# Git Clean branches, multichoice with main deselected by default
+function git-cl() {
+  # List all branches and space separate them
+  BRANCHES=$(git branch | grep -v HEAD | sed 's/[\*\s]*//' | tr -d ' ' | sort -u)
+  PRESELECTED=$(echo $BRANCHES | grep -v main | tr '\n' ',')
+
+  # Print selector
+  TO_DELETE="$(echo $BRANCHES | gum choose --no-limit --selected $PRESELECTED)"
+
+  # Delete branches with gum confirmation
+  echo $TO_DELETE | awk '{print " — " $0}' | gum style --foreground 212 --margin "2 0 1 0"
+  gum confirm "Delete branches?"  && git branch -d $TO_DELETE
+}
+
+# Git checkout branch using gum filter (fuzzy search)
+function git-co() {
+  BRANCHES=$(git branch | grep -v HEAD | sed 's/[\*\s]*//' | tr -d ' ' | sort -u)
+
+  # Gum filter branches
+  BRANCH=$(echo $BRANCHES | gum filter)
+
+  # Checkout branch
+  git checkout $BRANCH
+}
