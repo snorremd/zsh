f8e741b2ed6af37859103f1981231adda16cec13
Snorre Magnus Davøen <post@snorre.io>
2023-02-08 07:55:05 +0100

    Add spinners to gcloud-proxy alias
    To better communicate that it is actually processing (fetching projects
    or services from API) Gum spinners are now used while performing these
    operations.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- gcloud-aliases.zsh
+++ gcloud-aliases.zsh
@@ -3,11 +3,11 @@ PATH=$PATH:/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
 # Gcloud Proxy to proxy cloud run service to localhost
 function gc-proxy() {
   # Get list of and select project
-  PROJECTS=$(gcloud projects list --format='csv(project_id,name,project_number)' | sed 's/,/ - /g')
+  PROJECTS=$(gum spin --spinner line --title "Fetching projects" --show-output -- gcloud projects list --format='csv(project_id,name,project_number)' | sed 's/,/ - /g')
   PROJECT=$(echo $PROJECTS | gum filter | cut -d ' ' -f 1)
   
   # Get list of cloud run services and select one
-  SERVICES=$(gcloud run services list --project $PROJECT --format='csv(SERVICE,URL)' | sed 's/,/ - /g')
+  SERVICES=$(gum spin --spinner line --title "Fetching services" --show-output -- gcloud run services list --project $PROJECT --format='csv(SERVICE,URL)' | sed 's/,/ - /g')
   SERVICE_URL=$(echo $SERVICES | gum filter | cut -d ' ' -f 3)
   PORT=$(gum input --placeholder "Proxy to localhost port:")
   cloud-run-proxy -host $SERVICE_URL -bind localhost:$PORT
