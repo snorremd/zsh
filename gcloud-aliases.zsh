b008df1fb437a59f6f2aea42be4090102ae4d791
Snorre Magnus Davøen <post@snorre.io>
2023-03-01 10:20:06 +0100

    chore: Use gcloud beta run services proxy
    Proxying gcloud run services to localhost is now built into the gcloud
    command. The gc-proxy() alias now uses this new command to proxy. It
    sets the project and region arguments based on the selections done.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- gcloud-aliases.zsh
+++ gcloud-aliases.zsh
@@ -3,12 +3,32 @@ PATH=$PATH:/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
 # Gcloud Proxy to proxy cloud run service to localhost
 function gc-proxy() {
   # Get list of and select project
-  PROJECTS=$(gum spin --spinner line --title "Fetching projects" --show-output -- gcloud projects list --format='csv(project_id,name,project_number)' | sed 's/,/ - /g')
+  PROJECTS=$(
+    gum spin \
+      --spinner line \
+      --title "Fetching projects" \
+      --show-output \
+      -- gcloud projects list --format='csv(project_id,name,project_number)' \
+   | sed 's/,/ - /g' \
+   | tail -n +2)
+
   PROJECT=$(echo $PROJECTS | gum filter | cut -d ' ' -f 1)
-  
+
   # Get list of cloud run services and select one
-  SERVICES=$(gum spin --spinner line --title "Fetching services" --show-output -- gcloud run services list --project $PROJECT --format='csv(SERVICE,URL)' | sed 's/,/ - /g')
-  SERVICE_URL=$(echo $SERVICES | gum filter | cut -d ' ' -f 3)
+  SERVICES=$(
+    gum spin \
+      --spinner line \
+      --title "Fetching services" \
+      --show-output \
+      -- gcloud run services list --project $PROJECT --format='csv(SERVICE,REGION,URL)' \
+   | sed 's/,/ - /g' \
+   | tail -n +2)
+  SERVICE=$(echo $SERVICES | gum filter)
+  SERVICE_NAME=$(echo $SERVICE | sed 's/ - /./g' | cut -d '.' -f 1)
+  REGION=$(echo $SERVICE | sed 's/ - /./g' | cut -d '.' -f 2)
   PORT=$(gum input --placeholder "Proxy to localhost port:")
-  cloud-run-proxy -host $SERVICE_URL -bind localhost:$PORT
+
+  echo "Proxying $SERVICE_NAME to localhost:$PORT for project $PROJECT in region $REGION"
+
+  gcloud beta run services proxy $SERVICE_NAME --port $PORT --project $PROJECT --region $REGION
 }
