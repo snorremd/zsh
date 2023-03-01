9a3eefd5bf67a90b9a8b1d638daaa1cb697be252
Snorre Magnus Davøen <post@snorre.io>
2023-03-01 10:55:53 +0100

    feat: Add port-to-pid and kill-by-port aliases
    New port-to-pid alias to resolve a port number to a process id. Useful
    if you need to identify which program is using a given port.
    
    The kill-by-port alias allows you to kill a process using a port number.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- common-aliases.zsh
+++ common-aliases.zsh
@@ -3,4 +3,12 @@ function fuzzy() {
   $EDITOR $(gum filter)
 }
 
+function port-to-pid {
+	lsof -nP -iTCP -sTCP:LISTEN | grep $1 | tr -s ' ' | cut -d" " -f2
+}
+
+function kill-by-port {
+	kill -9 $(port-to-pid $1)
+}
+
 eval $(thefuck --alias)
