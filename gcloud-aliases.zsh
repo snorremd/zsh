96c4994ff653aba6ac98bfccb1162e4459745286
Snorre Magnus Davøen <post@snorre.io>
2023-05-08 12:46:11 +0200

    feat(gcloud): SQL Proxy wizard
    Add a new gum based prompt helper to get the correct gcloud sql
    connection name (project:region:name) to connect to. Makes it more
    convenient to use the cloud-sql-proxy command.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- gcloud-aliases.zsh
+++ gcloud-aliases.zsh
@@ -32,3 +32,38 @@ function gc-proxy() {
 
   gcloud beta run services proxy $SERVICE_NAME --port $PORT --project $PROJECT --region $REGION
 }
+
+
+# GCloud SQL Proxy to proxy cloud sql instance to localhost
+function gc-sql-proxy() {
+  # Get list of and select project
+  PROJECTS=$(
+    gum spin \
+      --spinner line \
+      --title "Fetching projects" \
+      --show-output \
+      -- gcloud projects list --format='csv(project_id,name,project_number)' \
+   | sed 's/,/ - /g' \
+   | tail -n +2)
+
+  PROJECT=$(echo $PROJECTS | gum filter | cut -d ' ' -f 1)
+
+  # Get list of cloud sql instances and select one
+  INSTANCES=$(
+    gum spin \
+      --spinner line \
+      --title "Fetching instances" \
+      --show-output \
+      -- gcloud sql instances list --project $PROJECT --format='csv(NAME,REGION)' \
+   | sed 's/,/ - /g' \
+   | tail -n +2)
+
+  INSTANCE=$(echo $INSTANCES | gum filter)
+
+  NAME=$(echo $INSTANCE | sed 's/ - /./g' | cut -d '.' -f 1)
+  ID=$(gcloud sql instances describe $NAME --project $PROJECT --format='value(connectionName)')
+
+  echo "Proxying $ID to localhost:5432"
+
+  cloud-sql-proxy $ID
+}
