b4359cd35b6fe8a3e44f91690cef8c250d6f33b7
set <set>
2023-11-27 14:21:48 +0100

    Make plugins check that necessary commands are installed
    If gum and babashka commands are not found, notify users.

--- aliases/aliases.plugin.zsh
+++ aliases/aliases.plugin.zsh
@@ -1,3 +1,12 @@
+# Check that bb command exists
+if ! command -v gum &> /dev/null
+then
+    echo "Could not load aliases zsh plugin"
+    echo "gum command could not be found"
+    echo "Please install gum: https://github.com/charmbracelet/gum"
+    return
+fi
+
 # Get directory of current script
 mypath=$0:A
 mypath=${mypath%/*}
--- bb-scripts/bb-scripts.plugin.zsh
+++ bb-scripts/bb-scripts.plugin.zsh
@@ -1,5 +1,15 @@
+# Check that bb command exists
+if ! command -v bb &> /dev/null
+then
+    echo "Could not load bb-scripts zsh plugin"
+    echo "bb command could not be found"
+    echo "Please install babashka: https://github.com/babashka/babashka#installation"
+    return
+fi
+
 # Add bb scripts to path
 mypath=$0:A
 mypath=${mypath%/*}
+
 
 export PATH="$PATH:$mypath"
